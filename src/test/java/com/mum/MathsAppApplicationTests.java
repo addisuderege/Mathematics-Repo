d6aa40cdde02c265649bbc985a5c89cdeee8ab60
agebru <amex2000@gmail.com>
2018-12-31 21:24:37 -0500

    update the controller --commented @Autowired

--- src/test/java/com/mum/MathsAppApplicationTests.java
+++ src/test/java/com/mum/MathsAppApplicationTests.java
@@ -5,10 +5,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.mum.service.CalculatorServiceImpl;
 import com.mum.service.ICalculatorService;
